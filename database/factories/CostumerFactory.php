519ae2cf3db3825609a577778657f2c70d91a03b
numaanjaved <naumaan.javed@hotmail.com>
2022-09-11 15:16:25 +0500

    costumer Factory created

--- database/factories/CostumerFactory.php
+++ database/factories/CostumerFactory.php
@@ -16,8 +16,16 @@ class CostumerFactory extends Factory
      */
     public function definition()
     {
+        $type = $this->faker->randomElement(['B', 'I']);
+        $name = $type == 'I' ? $this->faker->name() : $this->faker->company();
         return [
-            //
+            'name' => $name,
+            'type' => $type,
+            'email' => $this->faker->email(),
+            'address' => $this->faker->streetAddress(),
+            'city' => $this->faker->city() ,
+            'state' => $this->faker->state(),
+            'post_code' => $this->Faker->postCode(),
         ];
     }
 }
