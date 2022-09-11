7a753e0b676bfea235ec27feca7ee97b7fbc63a9
numaanjaved <naumaan.javed@hotmail.com>
2022-09-11 15:02:27 +0500

    created relations in Models

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Invoice extends Model
 {
     use HasFactory;
+
+    public function costumer(){
+        return $this->belongsTo(Costumer::class);
+    }
 }
